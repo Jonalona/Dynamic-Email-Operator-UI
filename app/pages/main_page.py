4e0889e08a1b03a60abaa0863e39643a5396ccb4
Jonalona <68975702+Jonalona@users.noreply.github.com>
2025-07-31 17:54:52 -0400

    Update main_page.py

--- app/pages/main_page.py
+++ app/pages/main_page.py
@@ -3,14 +3,9 @@ from dash import html, dcc, callback, Input, Output
 import dash_mantine_components as dmc
 from dash_iconify import DashIconify
 from reusable_components import create_new_user_component, create_view_users_button
-# Assume ALL_STATES is your list of DAG IDs
-ALL_STATES = [
-    "115_Daily_Performance_Email", "115_outbound_optimization_V2",
-    "407_Daily_Performance_Email", "436_Daily_Performance_Email",
-    "499_Daily_Performance_Email", "499_outbound_optimization_V2",
-    "712_Daily_Performance_Email", "BCG_Split", "Another_Example_DAG",
-    "Some_Really_Long_DAG_Name_To_Test_UI"
-]
+from global_vars import *
+# Assume ALL_DAGS is your list of DAG IDs
+
 
 dash.register_page(__name__, path='/', name="Home") # Example registration
 
@@ -57,9 +52,9 @@ def layout():
 )
 def update_button_list(filter_value):
     if not filter_value:
-        filtered_labels = ALL_STATES
+        filtered_labels = ALL_DAGS
     else:
-        filtered_labels = [label for label in ALL_STATES if filter_value.lower() in label.lower()]
+        filtered_labels = [label for label in ALL_DAGS if filter_value.lower() in label.lower()]
 
     if not filtered_labels:
         return dmc.Text("No DAGs found.", c="dimmed", p="lg")
